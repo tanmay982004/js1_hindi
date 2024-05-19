2d4788f5eace04bfefd73b55d2152bb3e58527a6
tanmay982004 <155573005+tanmay982004@users.noreply.github.com>
2024-05-19 21:09:48 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=14453
+let accountEmail="tanmay@gmail.com"
+
+var accountPassword="12345"
+accountCity="Patna"   
+
+let accountState;
+
+accountEmail="tanmayk572@gmail.com"
+accountPassword="12222"
+accountCity="Darbhanga"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var instead use let because
+of block scope versus functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js code as new version
+//alert(3+3) will not give result here
+//as we are using node js not browser
+
+// for standard documentation go to tc 39 and mdn
+
+let name="Tanmay"  //string data type
+let age=18       //number
+let isLoggedIn=false   //boolean
+
+//number=> 2 to power 53
+//bigInt
+//string=>""
+//boolean=> true /false
+
+//null=>standalone value  representaion of empty
+
+//undefined=>value is not assigned yet
+
+//symbol=>for uniqueness
+
+console.log(typeof age)  //number
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
+
