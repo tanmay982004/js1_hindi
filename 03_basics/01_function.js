4c642e09860747f77fa6c11d531df3d341365035
tanmay982004 <155573005+tanmay982004@users.noreply.github.com>
2024-06-18 11:10:00 +0000

    function 1 complete

---
+++ 03_basics/01_function.js
@@ -0,0 +1,31 @@
+//function is a block of code which execute some operation
+
+function sayMyName()
+{
+    console.log("Tanmay");
+}
+sayMyName()
+
+//return in function
+function add(number1,number2) //this is parameter
+{
+    // console.log(number1+number2);
+    return number1+number2;
+}
+// add(5,6);  //this is argument
+
+const result=add(5,6)
+console.log("Result:",result);
+
+function loginUserMessage(username="Ram") //we can provide username here in case user doesn't enter the name                                     //if he would enter the name then that name will override it
+{  
+    // if(!username)   
+    //     {
+    //         console.log("Please enter a username");   //this is the case if user has not given a username 
+    //         return       
+    //     }
+    return  `${username} just logged in`
+}
+
+console.log(loginUserMessage()); //if we don't pass anything it wil give us undefined
+           
