4cc36fd8f334693637c7273fd2f4673991d0d314
tanmay982004 <155573005+tanmay982004@users.noreply.github.com>
2024-06-21 23:05:53 +0000

    basic loop done

---
+++ 05_iteration/while_and_do_while.js
@@ -0,0 +1,12 @@
+let i=0
+while (i<3) {
+    console.log(i);
+    i++;
+}
+
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=5);  //this loop runs for once for sure
+
